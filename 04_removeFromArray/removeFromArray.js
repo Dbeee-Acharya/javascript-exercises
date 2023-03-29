a609d1a22e22341d6e5ad7e6f65286d9cea68555
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-03-29 21:31:15 +0545

    04 removeFromArray passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+/* TO DO:
+    1. Accept multiple arguments in a function
+    2. remove given elements
+    3. ignore elements that are not present
+    4. works with strings and data types
+*/ 
 
+const removeFromArray = function(array, ...toRemove) {
+    newArray = [];
+
+    newArray = array.filter( item => !toRemove.includes(item));
+
+    return newArray;
 };
 
 // Do not edit below this line
