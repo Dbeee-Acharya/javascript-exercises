3a81322ae9ce29151ef7a18e5001f4cfbb6cfaf3
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-03-30 13:29:56 +0545

    07 tempConversion complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+// F = (C * 9/5) + 32;
+// C = (F - 32) * 5/9;
+// Math.round(num * 100) / 100
+
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9
+  celsius = Math.round(celsius * 10) / 10;
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
