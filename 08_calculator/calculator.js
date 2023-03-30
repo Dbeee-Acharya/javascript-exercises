d3eac8a755f13dd01710740b43fb634938eebd3e
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-03-30 13:50:22 +0545

    08 calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  return (numOne + numTwo);
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return (numOne - numTwo);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let ans = 0;
+
+  for (number of array) {
+    ans += number;
+  }
+
+  return ans;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let ans = 1;
 
+  for (number of array) {
+    ans *= number;
+  }
+
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let ans = 1;
+	for (let i = 1; i <= num; i++) {
+    ans *= i;
+  }
+
+  return ans;
 };
 
 // Do not edit below this line
