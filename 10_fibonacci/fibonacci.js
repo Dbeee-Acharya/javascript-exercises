0e5dad69764f591026f2eef3f47fecb1ce18b574
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-04-01 11:53:00 +0545

    10 fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+// Fn = Fn-1 + Fn-2
 
+const fibonacci = function(number) {
+    const arr = [1,1]
+
+    if (number < 1) return "OOPS";
+
+    for (let i = 1; i <= +number; i++) {
+        arr.push(arr[i-1] + arr[i])
+    }
+
+    return arr[number-1];
 };
 
 // Do not edit below this line
